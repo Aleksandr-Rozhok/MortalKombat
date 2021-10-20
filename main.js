9cec6158b33a4eb11bd1d5574706c130cd38c5b6
Aleksandr <akkelaforest@gmail.com>
2021-10-20 21:39:43 +0300

    finished
    finish

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+const $arena = document.querySelector('.arenas');
+const $btn = document.querySelector('.button');
+const $chat = document.querySelector('.chat');
+
 const Scorpion = {
+    player: 1,
     name: 'Scorpion',
     hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
@@ -9,6 +14,7 @@ const Scorpion = {
 }
 
 const SubZero = {
+    player: 2,
     name: 'SubZero',
     hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
@@ -18,37 +24,72 @@ const SubZero = {
     }
 }
 
-const $arena = document.querySelector('.arenas');
+const createElement = (tag, className) => {
+    let $tag = document.createElement(tag);
 
-const createPlayer = (player, obj) => {
-    const $player = document.createElement('div');
-    $player.classList.add(player);
+    if (className) {
+        $tag.classList.add(className);
+    }
 
-    const $progressBar = document.createElement('div');
-    $progressBar.classList.add('progressbar');
+    return $tag;
+}
 
-    const $character = document.createElement('div');
-    $character.classList.add('character')
+const changeHP = (obj) => {
 
-    const $life = document.createElement('div');
-    $life.classList.add('life');
+
+
+    const randomizer = () => {
+        let $playerLife = obj.hp;
+        const $playerLifeIndicator = document.querySelector('.player' + obj.player + ' .life');
+        console.log($playerLifeIndicator);
+        console.log($playerLife);
+        min = 1;
+        max = 20;
+        let result = 0;
+        result = Math.floor(Math.random() * (max - min) + min);
+        $playerLifeIndicator.style.width = $playerLife + '%';
+        if ($playerLife <= 0) {
+            $playerLifeIndicator.style.width = 0 + '%';
+            $btn.disabled = true;
+            $btn.style.display = 'none';
+            const $winnerTitle = createElement('div', 'winnerTitle');
+            $chat.appendChild($winnerTitle);
+            if (SubZero.hp <= 0) {
+                $winnerTitle.innerHTML = 'Scorpion Win';
+            } else {
+                $winnerTitle.innerHTML = 'SubZero Win';
+            }
+        }
+        return (obj.hp = obj.hp - result);
+    }
+
+    $btn.addEventListener('click', randomizer)
+}
+
+const createPlayer = (obj) => {
+    const $player = createElement('div', 'player' + obj.player);
+    const $progressBar = createElement('div', 'progressbar');
+    const $character = createElement('div', 'character');
+    const $life = createElement('div', 'life');
     $life.style.width = obj.hp + '%';
 
-    const $name = document.createElement('div');
-    $name.classList.add('name');
+    const $name = createElement('div', 'name');
     $name.textContent = obj.name;
 
-    const $img = document.createElement('img');
+    const $img = createElement('img');
     $img.src = obj.img;
 
-    $arena.appendChild($player);
     $player.appendChild($progressBar);
     $player.appendChild($character);
     $progressBar.appendChild($life);
     $progressBar.appendChild($name);
     $character.appendChild($img);
 
+    return $player;
 }
 
-createPlayer('player1', Scorpion);
-createPlayer('player2', SubZero);
+$arena.appendChild(createPlayer(Scorpion));
+$arena.appendChild(createPlayer(SubZero));
+
+changeHP(SubZero);
+changeHP(Scorpion);
